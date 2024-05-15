52f2f820a782281b091af8ce4301ec58cad6b857
HimanshuSriv28 <Himanshusriv2000@gmail.com>
2024-05-15 23:43:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+
+
+const PORT = 8080;
+
+
+app.listen (
+    PORT,
+    () => console.log('server running on ${PORT}')
+);
+
+app.get('/fruits',(req,res) => {
+    res.send("Mang and banana")
+});
